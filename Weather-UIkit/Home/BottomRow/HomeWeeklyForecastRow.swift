5524334cc8001c3e57baa6561a62b3acb39f41cf
Aggelosk <akermezo@yahoo.com>
2024-11-12 12:00:48 +0200

    Home page,bottom row
    table view,slider,labels,img

--- Weather-UIkit/Home/BottomRow/HomeWeeklyForecastRow.swift
+++ Weather-UIkit/Home/BottomRow/HomeWeeklyForecastRow.swift
@@ -10,9 +10,12 @@ import UIKit
 class HomeWeeklyForecastRow: UITableViewCell {
     static let id = "HomeWeeklyForecastRow"
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        setupTableView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -20,5 +23,31 @@ class HomeWeeklyForecastRow: UITableViewCell {
 
         
     }
+    
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func configure() {
+        
+    }
 
+}
+
+extension HomeWeeklyForecastRow: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: WeeklyForecastDetailRow.id, for: indexPath) as! WeeklyForecastDetailRow
+        return cell
+    }
+}
+
+extension HomeWeeklyForecastRow: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
 }
